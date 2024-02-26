c861651ae63c5e9d33bb4c884a9f22d46f5c0c7b
alocke8181 <alocke_8181@yahoo.com>
2024-02-26 14:04:47 -0500

    added limits to bio textareas

--- src/Character/CharacterBio.js
+++ src/Character/CharacterBio.js
@@ -28,44 +28,52 @@ const CharacterBio = ({setCharacter})=>{
                 <div id="character-bio-pibf">
                     <div className="character-bio-subcont">
                         <label htmlFor="personality"><h3>Personality</h3></label>
+                        <i>{bioFormData.personality.length}/300</i>
                         <p>
                             <textarea
                                 id="personality"
                                 name="personality"
                                 value={bioFormData.personality}
+                                maxLength='300'
                                 onChange={handleChange}
                             />
                         </p>
                     </div>
                     <div className="character-bio-subcont">
                         <label htmlFor="ideals"><h3>Ideals</h3></label>
+                        <i>{bioFormData.ideals.length}/300</i>
                         <p>
                             <textarea
                                 id="ideals"
                                 name="ideals"
                                 value={bioFormData.ideals}
+                                maxLength='300'
                                 onChange={handleChange}
                             />
                         </p>
                     </div>
                     <div className="character-bio-subcont">
                         <label htmlFor="bonds"><h3>Bonds</h3></label>
+                        <i>{bioFormData.bonds.length}/300</i>
                         <p>
                             <textarea
                                 id="bonds"
                                 name="bonds"
                                 value={bioFormData.bonds}
+                                maxLength='300'
                                 onChange={handleChange}
                             />
                         </p>
                     </div>
                     <div className="character-bio-subcont">
                         <label htmlFor="flaws"><h3>Flaws</h3></label>
+                        <i>{bioFormData.flaws.length}/300</i>
                         <p>
                             <textarea
                                 id="flaws"
                                 name="flaws"
                                 value={bioFormData.flaws}
+                                maxLength='300'
                                 onChange={handleChange}
                             />
                         </p>
@@ -89,6 +97,7 @@ const CharacterBio = ({setCharacter})=>{
                                 type="text"
                                 id="height"
                                 name="height"
+                                maxLength='20'
                                 value={bioFormData.height}
                                 onChange={handleChange}
                             />
@@ -100,17 +109,20 @@ const CharacterBio = ({setCharacter})=>{
                                 id="weight"
                                 name="weight"
                                 value={bioFormData.weight}
+                                maxLength='20'
                                 onChange={handleChange}
                             />
                         </p>
                     </div>
                     <div id="character-bio-appearance">
                         <label htmlFor="appearance"><h3>Appearance</h3></label>
+                        <i>{bioFormData.appearance.length}/500</i>
                         <p>
                             <textarea
                                 id="appearance"
                                 name="appearance"
                                 value={bioFormData.appearance}
+                                maxLength='500'
                                 onChange={handleChange}
                             />
                         </p>
@@ -118,10 +130,12 @@ const CharacterBio = ({setCharacter})=>{
                 </div>
                 <div id="character-bio-backstory">
                         <label htmlFor="backstory"><h3>Backstory</h3></label>
+                        <i>{bioFormData.backstory.length}/1000</i>
                         <p>
                             <textarea
                                 id="backstory"
                                 name="backstory"
+                                maxLength='1000'
                                 value={bioFormData.backstory}
                                 onChange={handleChange}
                             />
@@ -129,10 +143,12 @@ const CharacterBio = ({setCharacter})=>{
                     </div>
                 <div id="character-bio-allies">
                     <label htmlFor="allies"><h3>Allies</h3></label>
+                    <i>{bioFormData.allies.length}/500</i>
                     <p>
                         <textarea
                             id="allies"
                             name="allies"
+                            maxLength='500'
                             value={bioFormData.allies}
                             onChange={handleChange}
                         />
